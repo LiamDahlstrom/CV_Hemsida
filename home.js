7d275627f26b869a83339c76bc15c09f49be1927
Liam Dahlström <idrelimpan@gmail.com>
2022-03-16 12:40:44 +0700

    about, portfolio

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 var x = 0
 
-function clickCount() {
+function clickCount1() {
     x += 1;
     clicks = "click " + x;
 
@@ -9,27 +9,84 @@ switch(clicks) {
 
         document.getElementById('work1').style.cssText = 'height: 80vh; margin-top: 10vh;'
 
-        document.getElementById('txt_work').style.cssText = 'opacity: 100%; height: 0%'
+        document.getElementById('txt_work1').style.cssText = 'opacity: 100%; height: 0%'
       
 break;
     case "click 2":
             document.getElementById('work1').style.cssText = 'height: 10vh; margin-top: 45vh;'
 
-            document.getElementById('txt_work').style.cssText = 'opacity: 0%; height: 100%'
+            document.getElementById('txt_work1').style.cssText = 'opacity: 0%; height: 100%'
 
             x = 0
       break;
 }
 }
+var x = 0
 
+function clickCount2() {
+    x += 1;
+    clicks = "click " + x;
 
+switch(clicks) {
+    case "click 1":
 
-function work2() {
-document.getElementById('work2').style.cssText = 'height: 80vh; margin-top: 10vh;'
+        document.getElementById('work2').style.cssText = 'height: 80vh; margin-top: 10vh;'
+
+        document.getElementById('txt_work2').style.cssText = 'opacity: 100%; height: 0%'
+      
+break;
+    case "click 2":
+            document.getElementById('work2').style.cssText = 'height: 10vh; margin-top: 45vh;'
+
+            document.getElementById('txt_work2').style.cssText = 'opacity: 0%; height: 100%'
+
+            x = 0
+      break;
 }
-function work3() {
-document.getElementById('work3').style.cssText = 'height: 80vh; margin-top: 10vh;'
 }
-function work4() {
-document.getElementById('work4').style.cssText = 'height: 80vh; margin-top: 10vh;'
+var x = 0
+
+function clickCount3() {
+    x += 1;
+    clicks = "click " + x;
+
+switch(clicks) {
+    case "click 1":
+
+        document.getElementById('work3').style.cssText = 'height: 80vh; margin-top: 10vh;'
+
+        document.getElementById('txt_work3').style.cssText = 'opacity: 100%; height: 0%'
+      
+break;
+    case "click 2":
+            document.getElementById('work3').style.cssText = 'height: 10vh; margin-top: 45vh;'
+
+            document.getElementById('txt_work3').style.cssText = 'opacity: 0%; height: 100%'
+
+            x = 0
+      break;
+}
+}
+var x = 0
+
+function clickCount4() {
+    x += 1;
+    clicks = "click " + x;
+
+switch(clicks) {
+    case "click 1":
+
+        document.getElementById('work4').style.cssText = 'height: 80vh; margin-top: 10vh;'
+
+        document.getElementById('txt_work4').style.cssText = 'opacity: 100%; height: 0%'
+      
+break;
+    case "click 2":
+            document.getElementById('work4').style.cssText = 'height: 10vh; margin-top: 45vh;'
+
+            document.getElementById('txt_work4').style.cssText = 'opacity: 0%; height: 100%'
+
+            x = 0
+      break;
+}
 }
