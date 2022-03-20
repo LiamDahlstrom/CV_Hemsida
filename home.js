677d53533ecb57a2e2a37ed3006843d49ccc2206
Liam Dahlström <idrelimpan@gmail.com>
2022-03-21 00:32:52 +0700

    work, style, aducation, contact and more

--- home.js
+++ home.js
@@ -9,7 +9,7 @@ switch(clicks) {
 
         document.getElementById('work1').style.cssText = 'height: 80vh; margin-top: 10vh;'
 
-        document.getElementById('txt_work1').style.cssText = 'opacity: 100%; height: 0%'
+        document.getElementById('txt_work1').style.cssText = 'opacity: 100%; height: 60vh'
       
 break;
     case "click 2":
@@ -32,7 +32,7 @@ switch(clicks) {
 
         document.getElementById('work2').style.cssText = 'height: 80vh; margin-top: 10vh;'
 
-        document.getElementById('txt_work2').style.cssText = 'opacity: 100%; height: 0%'
+        document.getElementById('txt_work2').style.cssText = 'opacity: 100%; height: 80vh'
       
 break;
     case "click 2":
@@ -55,7 +55,7 @@ switch(clicks) {
 
         document.getElementById('work3').style.cssText = 'height: 80vh; margin-top: 10vh;'
 
-        document.getElementById('txt_work3').style.cssText = 'opacity: 100%; height: 0%'
+        document.getElementById('txt_work3').style.cssText = 'opacity: 100%; height: 80vh'
       
 break;
     case "click 2":
@@ -78,7 +78,7 @@ switch(clicks) {
 
         document.getElementById('work4').style.cssText = 'height: 80vh; margin-top: 10vh;'
 
-        document.getElementById('txt_work4').style.cssText = 'opacity: 100%; height: 0%'
+        document.getElementById('txt_work4').style.cssText = 'opacity: 100%; height: 80vh'
       
 break;
     case "click 2":
